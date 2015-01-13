a2efe6419fd8ed8035124252e2c9bab9e959823d
Denis Ollier <larchunix@users.noreply.github.com>
2015-01-13 20:03:55 +0200

    build: add autoconf initialization file

--- src/rpmatch.c
+++ src/rpmatch.c
@@ -27,6 +27,10 @@
  *   -v, --version  Display version information and exit
  */
 
+#ifdef HAVE_CONFIG_H
+#  include "config.h"  /* PACKAGE_STRING */
+#endif /* HAVE_CONFIG_H */
+
 #define _SVID_SOURCE     /* rpmatch(3) is exposed through the SysV Interface Definition */
 #define _DEFAULT_SOURCE  /* _SVID_SOURCE is replaced by _DEFAULT_SOURCE since glibc 2.19 */
 
@@ -71,7 +75,7 @@ rpmatch_usage(const char *argv0, FILE *stream)
 static void
 rpmatch_version(void)
 {
-    fputs("rpmatch 1.0.0-rc1\n", stdout);
+    fputs(PACKAGE_STRING"\n", stdout);
 }
 
 static void
